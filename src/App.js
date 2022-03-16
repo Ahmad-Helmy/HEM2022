8a298036b831d6f48b84adc55bb9987d9024a744
Ahmed Helmy <ahmhelmhalm@gmail.com>
2022-03-16 15:57:40 +0200

    feat: group photo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div>
-      
+      <img src='https://drive.google.com/uc?export=view&id=16nZUZCIfL71vcEMALlm5IE8ZQNUN2Ui4' />
+
     </div>
   );
 }
